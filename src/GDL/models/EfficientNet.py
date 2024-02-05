7f46151ba013a4ab4a3a4e24066c8b02491e3990
Yunheng Wang <wangyunhenggxy@zjnu.edu.cn>
2024-02-05 19:48:48 +0800

    2024-02-05 Create EfficientNet models.

---
+++ src/GDL/models/EfficientNet.py
@@ -0,0 +1,24 @@
+#
+#    paper name : EfficientNet: Rethinking Model Scaling for Convolutional Neural Networks
+# paper address : https://arxiv.org/abs/1905.11946
+#    paper code : https://github.com/tensorflow/tpu/tree/master/models/official/efficientnet
+#
+# ==========================================================================================
+
+""" EfficientNet model
+
+The architecture used to define the EfficientNet model
+"""
+
+
+class EfficientNetV1():
+    """"""
+    def __init__(self):
+        pass
+
+    def call(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
